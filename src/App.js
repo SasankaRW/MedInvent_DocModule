15df2a858132b8b2eb4f3a767c5be6620ae53afc
Supun D <supundunusingha2@gmail.com>
2024-02-12 14:31:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,6 +16,25 @@ function App() {
     setActiveSubMenu(subMenuText);
   };
 
+  const renderSubMenuComponent = () => {
+    switch (activeSubMenu) {
+      case "All Doctors":
+        return <AllDoctors />;
+      case "Add New Doctor":
+        return <NewDoctorForm />;
+      case "All Clinics":
+        return <AllClinics />;
+      case "All Pharmacies":
+        return <AllPharmacies />;
+      case "Add New Clinic":
+        return <NewClinicForm />;
+      case "Add New Pharmacy":
+        return <NewPharmacyForm />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="App">
       <div>
@@ -23,10 +42,7 @@ function App() {
           onSubMenuClick={handleSubMenuClick}
           activeSubMenu={activeSubMenu}
         >
-          {" "}
-          <Form title={activeSubMenu}>
-            <AllDoctors />
-          </Form>
+          <Form title={activeSubMenu}>{renderSubMenuComponent()}</Form>
         </Sidebar>
       </div>
     </div>
