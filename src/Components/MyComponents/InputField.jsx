49ffbba16e06e42f9816a8f24aac61148e6da8f8
Nithila Dilshika <nithilamendis123@gmail.com>
2024-02-13 02:24:17 +0530

    AddPharmacyForm_firstTwoLabels

--- src/Components/MyComponents/InputField.jsx
+++ src/Components/MyComponents/InputField.jsx
@@ -3,7 +3,6 @@ import "./InputField.css";
 export default function InputField({ placeholder, label }) {
   return (
     <div className="input-container">
-      <label>{label}</label>
       <input type="text" className="input-field" placeholder={placeholder} />
     </div>
   );
--- src/pages/pharmacies/AddNewPharmacy.jsx
+++ src/pages/pharmacies/AddNewPharmacy.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import "../../Components/MyComponents/Container.css";
+import './AddNewPharmacy.css';
+import InputField from '../../Components/MyComponents/InputField';
+
 
 export default function NewPharmacyForm() {
   return (
     <div className="cont">
       <div className="cont-title">New Pharmacy</div>
-      <div>New Pharmacy</div>
+      
+      <div className="labelContainer">
+      <div className="pharmacyLabel">
+        <label className="label">Pharmacy Name</label>
+        <InputField label="Pharmacy Name" placeholder="Enter pharmacy name" />
+      </div>
+
+      <div className="contactLabel">
+        <label className="label">Contact Number</label>
+        <InputField label="Conatact Number" placeholder="Enter the contact number" />
+      </div>
+      </div>
+
     </div>
   );
 }
