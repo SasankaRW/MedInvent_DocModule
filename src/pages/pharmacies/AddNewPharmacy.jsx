678dcacec54e4cb71d648ecddd01208c664c781f
Supun D <supundunusingha2@gmail.com>
2024-02-13 23:16:47 +0530

    font changed and grid added

--- src/pages/pharmacies/AddNewPharmacy.jsx
+++ src/pages/pharmacies/AddNewPharmacy.jsx
@@ -1,26 +1,42 @@
 import React from "react";
 import "../../Components/MyComponents/Container.css";
-import './AddNewPharmacy.css';
-import InputField from '../../Components/MyComponents/InputField';
-
+import "./AddNewPharmacy.css";
+import InputField from "../../Components/MyComponents/InputField";
+import { Grid } from "@mui/material";
 
 export default function NewPharmacyForm() {
   return (
     <div className="cont">
       <div className="cont-title">New Pharmacy</div>
-      
-      <div className="labelContainer">
-      <div className="pharmacyLabel">
-        <label className="label">Pharmacy Name</label>
-        <InputField label="Pharmacy Name" placeholder="Enter pharmacy name" />
-      </div>
 
-      <div className="contactLabel">
-        <label className="label">Contact Number</label>
-        <InputField label="Conatact Number" placeholder="Enter the contact number" />
-      </div>
-      </div>
-
+      <form className="new-pharmacy-form">
+        <Grid container rowSpacing={5} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={6}>
+            <div className="pharmacyLabel">
+              <label className="label">Pharmacy Name</label>
+              <InputField
+                label="Pharmacy Name"
+                placeholder="Enter pharmacy name"
+              />
+            </div>
+          </Grid>
+          <Grid item xs={6}>
+            <div className="contactLabel">
+              <label className="label">Contact Number</label>
+              <InputField
+                label="Conatact Number"
+                placeholder="Enter the contact number"
+              />
+            </div>
+          </Grid>
+          <Grid item xs={6}>
+            <div>item</div>
+          </Grid>
+          <Grid item xs={6}>
+            <div>item</div>
+          </Grid>
+        </Grid>
+      </form>
     </div>
   );
 }
