ee62fd7e08a8ce4193e68ddb02767279dc9946d9
Supun D <supundunusingha2@gmail.com>
2024-02-20 22:04:24 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { NavigationProvider } from './context/navigation';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { NavigationProvider } from "./context/navigation";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <NavigationProvider>
@@ -13,9 +12,3 @@ root.render(
     </NavigationProvider>
   </React.StrictMode>
 );
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
