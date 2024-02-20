6f93110104b2d2beca49d3d429580d621e7558fc
Supun D <supundunusingha2@gmail.com>
2024-02-21 00:20:27 +0530

    MUI input fields added

--- src/Components/MyComponents/InputField.jsx
+++
@@ -1,9 +0,0 @@
-import "./InputField.css";
-
-export default function InputField({ placeholder, label }) {
-  return (
-    <div className="input-container">
-      <input type="text" className="input-field" placeholder={placeholder} />
-    </div>
-  );
-}
--- src/pages/pharmacies/AddNewPharmacy.jsx
+++ src/pages/pharmacies/AddNewPharmacy.jsx
@@ -1,40 +1,41 @@
 import React from "react";
 import Container from "../../Components/MyComponents/Container";
 import Paper from "@mui/material/Paper";
-import "./AddNewPharmacy.css";
-import InputField from "../../Components/MyComponents/InputField";
 import { Grid } from "@mui/material";
+import TextField from "@mui/material/TextField";
 
 export default function NewPharmacyForm() {
   return (
     <Paper elevation={5} sx={{ borderRadius: "10px" }}>
       <Container title="New Pharmacy">
-        <form className="new-pharmacy-form">
+        <form style={{ margin: "50px 0px" }}>
           <Grid
             container
             rowSpacing={5}
             columnSpacing={{ xs: 1, sm: 2, md: 3 }}
           >
             <Grid item xs={6}>
-              <div className="pharmacyLabel">
-                <label className="label">Pharmacy Name</label>
-                <InputField
+              <div style={{ marginRight: "150px" }}>
+                <TextField
                   label="Pharmacy Name"
-                  placeholder="Enter pharmacy name"
+                  placeholder="Pharmacy Name"
+                  variant="outlined"
+                  fullWidth
                 />
               </div>
             </Grid>
             <Grid item xs={6}>
-              <div className="contactLabel">
-                <label className="label">Contact Number</label>
-                <InputField
-                  label="Conatact Number"
-                  placeholder="Enter the contact number"
+              <div style={{ marginRight: "150px" }}>
+                <TextField
+                  label="Contact Number"
+                  placeholder="Contact Number"
+                  variant="outlined"
+                  fullWidth
                 />
               </div>
             </Grid>
             <Grid item xs={6}>
-              <div>item</div>
+              item
             </Grid>
             <Grid item xs={6}>
               <div>item</div>
