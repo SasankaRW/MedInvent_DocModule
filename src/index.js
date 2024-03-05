d9b655ef7da45f85bb504c63e907d39ea6978f76
Supun D <supundunusingha2@gmail.com>
2024-03-05 20:03:32 +0530

    bootstrap added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { NavigationProvider } from "./views/admin/context/navigation";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
